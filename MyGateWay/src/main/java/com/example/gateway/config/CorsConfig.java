cf9c5ddd2fc35c31dd69712578b97893e488a13c
dengchao <dengchao3119@163.com>
2023-05-12 13:40:53 +0800

    添加跨域和异常处理

---
+++ MyGateWay/src/main/java/com/example/gateway/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Description: 网关解决跨域问题
+ * @author:dengchao
+ * @Date: 2023-05-12 13:20
+ */
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
